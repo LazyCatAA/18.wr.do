2dfc4e5558ed9d30c667a0aff62e8a1d43d5b9b0
LazyCatAA <luyun199838@gmail.com>
2025-10-16 15:04:30 +0800

    Update text in About component

--- routes/7e4288fd5150d5dadda5aaf8e553d201.tsx
+++ routes/7e4288fd5150d5dadda5aaf8e553d201.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div class="w-full p-2 leading-8 min-h-screen text-shadow bg-pink-300 text-lg text-red-100">
       <h1 class="my-8 text-sm text-center font-bold text-[#c5005a]">
-        18.wr.do 情趣小游戏
+        LazyCat 情趣小游戏
       </h1>
       <img src="/logo.png" class="w-16 h-16 mx-auto rounded-lg" />
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -17,7 +17,7 @@ export default function About() {
           激活会员后将自动跳转回首页...
         </p>
         <p class="fixed bottom-20 left-0 right-0 text-center m-2 font-bold text-[#c5005a]">
-          🔔 有问题请添加客服微信【 oiovdev 】
+          🔔 有问题请添加客服微信【 YunYun038a 】
         </p>
       </div>
 
